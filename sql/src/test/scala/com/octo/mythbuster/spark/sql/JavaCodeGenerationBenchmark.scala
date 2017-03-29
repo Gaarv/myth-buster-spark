623ee947dbf92b08c0b36a2597fc7337cf517e21
BES <bes@octo.com>
2017-03-29 12:18:09 +0200

    Add base of benchmarks

---
+++ sql/src/test/scala/com/octo/mythbuster/spark/sql/JavaCodeGenerationBenchmark.scala
@@ -0,0 +1,33 @@
+package com.octo.mythbuster.spark.sql
+
+import org.scalameter.api._
+
+class JavaCodeGenerationBenchmark extends Bench.LocalTime {
+
+  val sql = """SELECT
+              |  ca.name AS name
+              |FROM
+              |  cars ca
+              |JOIN
+              |  companies co
+              |ON
+              |  ca.company_id = co.id
+              |WHERE
+              |  co.name = 'Toyota'
+            """.stripMargin
+
+  val sizes = Gen.range("size")(300000, 1500000, 300000)
+
+  val ranges = for {
+    size <- sizes
+  } yield 0 until size
+
+  performance of "JavaCodeGeneration" in {
+    measure method "execute" in {
+      using(ranges) in {
+        val query = Query
+      }
+    }
+  }
+
+}
